9f452ab77f0223d6cf07917db184c9235c7e9c5e
Dhaval Jardosh <dhavaljardosh@csu.fullerton.edu>
2017-10-31 06:09:58 -0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import Searchbar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
+
+const API_KEY = 'AIzaSyAIVtRs2cddQP7DZRMcDUHdCRWYdNPmgUw';
+
+
+
+
+// Create a new component. This component should produce some HTML
+class App extends Component{
+
+  constructor(props){
+    super(props);
+    this.state = { videos = [] };
+
+    YTSearch({key: API_KEY, term: {this.state.videos}}, (data) => {
+      this.setState({videos: data});
+    });
+  }
+
+  render(){
+    return(
+      <div>
+        <Searchbar />
+      </div>
+    );
+  }
+}
+
+
+// Take this component, generated HTML and put it on the page(in the DOM)
+
+ReactDOM.render(<App />, document.querySelector('.container'));
